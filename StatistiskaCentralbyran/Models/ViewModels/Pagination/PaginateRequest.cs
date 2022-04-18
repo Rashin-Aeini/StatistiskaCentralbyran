c416df535d85c3cdeb991690097c5d515bc57213
Rashin-Aeini <aeini.rashin@gmail.com>
2022-04-18 11:30:01 +0200

    Create base class for pagination

---
+++ StatistiskaCentralbyran/Models/ViewModels/Pagination/PaginateRequest.cs
@@ -0,0 +1,42 @@
+﻿namespace StatistiskaCentralbyran.Models.ViewModels.Pagination
+{
+    public class PaginateRequest
+    {
+        private int _page;
+        private int _size;
+
+        public int Page
+        {
+            get
+            {
+                if (_page == 0)
+                {
+                    _page = 1;
+                }
+
+                return _page;
+            }
+            set
+            {
+                _page = value;
+            }
+        }
+
+        public int Size
+        {
+            get
+            {
+                if (_size == 0)
+                {
+                    _size = 10;
+                }
+
+                return _size;
+            }
+            set
+            {
+                _size = value;
+            }
+        }
+    }
+}
---
+++ StatistiskaCentralbyran/Models/ViewModels/Pagination/PaginateResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace StatistiskaCentralbyran.Models.ViewModels.Pagination
+{
+    public abstract class PaginateResponse<T> where T : class
+    {
+        public int TotalRecordes { get; set; }
+        public int TotalPages { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public List<T> Recordes { get; set; }
+    }
+}
