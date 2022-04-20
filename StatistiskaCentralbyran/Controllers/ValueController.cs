c0f3b0898c43a2ec797a393c7936c314600f6df5
Rashin-Aeini <aeini.rashin@gmail.com>
2022-04-20 19:00:08 +0200

    Change Controllers organization

---
+++ StatistiskaCentralbyran/Controllers/ValueController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StatistiskaCentralbyran.Models.Domains;
+using StatistiskaCentralbyran.Models.Interfaces;
+using StatistiskaCentralbyran.Models.ViewModels.Pagination;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatistiskaCentralbyran.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        private IYearService YearService { get; }
+        private IRegionService RegionService { get; }
+
+        public ValueController(IYearService yearService, IRegionService regionService)
+        {
+            YearService =yearService;
+            RegionService = regionService;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Year(PaginateRequest request)
+        {
+            try
+            {
+                return Ok(await YearService.ListAsync(request));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Region(PaginateRequest request)
+        {
+            try
+            {
+                return Ok(await RegionService.ListAsync(request));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Gender(PaginateRequest request)
+        {
+            return Ok(Enum.GetNames(typeof(Gender)).ToList());
+        }
+    }
+}
